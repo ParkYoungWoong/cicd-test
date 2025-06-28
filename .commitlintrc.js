71c90d4127b1dc5dff9db3bffa897ad625ffc7d1
ParkYoungWoong <thesecon@gmail.com>
2025-06-28 10:55:12 +0900

    테스트..

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,4 +1,5 @@
 export default {
+  extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [0, 'always', ['feat', 'fix', 'docs', 'refactor', 'test']],
     'header-max-length': [2, 'always', 72]
