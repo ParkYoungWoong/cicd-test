2dc613543fe7ba9b1b5a6d7383565e0b4fed9130
ParkYoungWoong <thesecon@gmail.com>
2025-06-28 11:00:20 +0900

    테스트..

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,13 +1,5 @@
-import { RuleConfigSeverity } from '@commitlint/types'
-
 export default {
-  extends: [],
   rules: {
-    'type-enum': [
-      RuleConfigSeverity.Error,
-      'always',
-      ['feat', 'fix', 'docs', 'refactor', 'test']
-    ],
-    'header-max-length': [RuleConfigSeverity.Error, 'always', 72]
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'refactor', 'test']]
   }
 }
